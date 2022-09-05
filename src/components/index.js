9ab2f5e1e5ccf6a4e5b5f8d7f532e612e259bc2c
Priydarshini Karanth <kpriya1203@gmail.com>
2022-09-05 15:41:55 +0530

    about page complete

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Navbar from './Navbar';
 import Footer from './Footer';
 import Sidebar from './Sidebar';
+import PageHero from './PageHero';
 
-export { Footer, Navbar, Sidebar };
+export { Footer, Navbar, Sidebar, PageHero };
