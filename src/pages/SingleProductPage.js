e98fedc74117f364ac87ec5c7565771ccb6b72a5
Priydarshini Karanth <kpriya1203@gmail.com>
2022-09-14 15:13:49 +0530

    error handling on single product page

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -1,19 +1,31 @@
 import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { Error, Loading } from '../components';
 import { useProductsContext } from '../context/products_context';
 import { single_product_url as url } from '../utils/constants';
 
 const SingleProductPage = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
   const {
     fetchSingleProduct,
     single_product_error: error,
     single_product_loading: loading,
   } = useProductsContext();
-  const { id } = useParams();
+
+  // fetch product details
   useEffect(() => {
     fetchSingleProduct(`${url}${id}`);
   }, [id]);
+
+  // redirect after 3 seconds in case of an error
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        navigate('/');
+      }, 3000);
+    }
+  }, [error]);
 
   if (loading) {
     return <Loading />;
