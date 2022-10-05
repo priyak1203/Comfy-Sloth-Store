bb98f2ec2031ca5ebcc905550d689fafaa7094fc
Priydarshini Karanth <kpriya1203@gmail.com>
2022-10-05 13:57:51 +0530

    colors in AddToCart component

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -84,7 +84,7 @@ const SingleProductPage = () => {
               {company}
             </p>
             <hr />
-            {stock > 0 && <AddToCart />}
+            {stock > 0 && <AddToCart {...product} />}
           </section>
         </div>
       </div>
