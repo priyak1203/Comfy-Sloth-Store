a9b77f405e5c6e547adb261c84479b5160f0d2fd
Priydarshini Karanth <kpriya1203@gmail.com>
2022-10-20 11:46:38 +0530

    cart page basic setup implementation

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,16 +1,39 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { CartContent, PageHero } from '../components';
+import { useCartContext } from '../context/cart_context';
 
 const CartPage = () => {
+  const { cart } = useCartContext();
+
+  if (cart.length < 1) {
+    return (
+      <main className="page-100">
+        <Wrapper>
+          <h2>Your cart is empty</h2>
+          <Link to="/products" className="btn">
+            fill it
+          </Link>
+        </Wrapper>
+      </main>
+    );
+  }
   return (
-    <Wrapper>
+    <main>
       <PageHero title="cart" />
-      <CartContent />
-    </Wrapper>
+      <Wrapper className="page">
+        <CartContent />
+      </Wrapper>
+    </main>
   );
 };
 
-const Wrapper = styled.main`
-  background-color: pink;
+const Wrapper = styled.section`
+  text-align: center;
+
+  h2 {
+    text-transform: none;
+    margin-bottom: 1rem;
+  }
 `;
 export default CartPage;
